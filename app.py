a643147d7954abc9674904418458fd6ded9f3176
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-01 10:17:08 +0900

    feat: 예약 테이블 주문 접수, 취소 구현

--- app.py
+++ app.py
@@ -1,11 +1,69 @@
 from flask import Flask, request, jsonify
 import login_test  # 이것은 앞에서 만든 Selenium 스크립트입니다.
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/reservation', method=['POST'])
+# Function to insert data into the Reservation table
+def insert_reservation_data(id, pw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck):
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    # Prepare the SQL query
+    query = """
+    INSERT INTO Reservation (uid, upw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """
+
+    # Execute the query with the data provided
+    cursor.execute(query, (id, pw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+    
+
+# Function to retrieve all rows from the Reservation table
+def get_reservation_data():
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    # Prepare the SQL query
+    query = """
+    SELECT * FROM Reservation
+    """
+
+    # Execute the query and fetch all rows
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    return rows
+
+# Function to delete a reservation from the Reservation table
+def delete_reservation_data(id):
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    # Prepare the SQL query
+    query = """
+    DELETE FROM Reservation WHERE id=?
+    """
+
+    # Execute the query with the id provided
+    cursor.execute(query, (id,))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
+
+@app.route('/reservation', methods=['POST'])
 def reservation_route():
     try:
         print(request.form)
@@ -18,11 +76,46 @@ def reservation_route():
         futureTime = request.form.get('futureTime')
         wednesdayCheck = request.form.get('wednesdayCheck')
         
+        # sqlite db 연결
+        insert_reservation_data(id, pw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck)
+
+        return jsonify({'success': True}), 200
+        
     except Exception as e:
         error_message = str(e)
         response = {'success': False, 'error': error_message}
         return jsonify(response), 500
+    
+@app.route('/reservation_table', methods=['GET'])
+def reservation_table():
+    try:
+        # Get all rows from the Reservation table
+        reservation_data = get_reservation_data()
 
+        # Convert the data into a list of dictionaries (JSON format)
+        reservations = []
+        for row in reservation_data:
+            reservation = {
+                'id': row[0],
+                'uid': row[1],
+                'upw': row[2],
+                'personnel': row[3],
+                'nextFuture': row[4],
+                'nextSaturday': row[5],
+                'nextSunday': row[6],
+                'futureTime': row[7],
+                'wednesdayCheck': row[8],
+            }
+            reservations.append(reservation)
+
+        # Return the data as a JSON response
+        return jsonify({'reservations': reservations}), 200
+
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
+    
 
 @app.route('/login', methods=['POST'])
 def login_route():
@@ -48,13 +141,18 @@ def login_route():
         return jsonify(response), 500
 
 
-# @celery.task()
-def run_login_test(id, pw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck):
-    cookies, elapsed_time = login_test.login_test(
-        "https://www.debeach.co.kr/", id, pw, personnel, nextFuture, nextSaturday, nextSunday, futureTime, wednesdayCheck)
+@app.route('/reservation_cancel/<int:id>', methods=['DELETE'])
+def reservation_cancel_route(id):
+    try:
+        # Delete the reservation with the provided id
+        delete_reservation_data(id)
 
-    return str(elapsed_time)
+        return jsonify({'success': True}), 200
 
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
 
 if __name__ == '__main__':
     app.debug = True
