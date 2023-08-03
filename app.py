a207206b1593befec2a869fbfaed636bec3a865e
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-08-03 11:21:41 +0900

    fix: 매일 09:00에 예약 명령 수행

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 # Your existing Flask code...
 
+
 def check_and_delete_reservations():
     try:
         # Get the current date and time
@@ -18,26 +19,29 @@ def check_and_delete_reservations():
         print(reservation_data)
         # Iterate through the rows to check conditions and delete if needed
         for row in reservation_data:
-            next_future_date_str = row[4]  # Assuming nextFuture is a string representing a date
-            future_time_str = row[5]       # Assuming futureTime is a string representing time
+            selectedDay = datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f')
+            # next_future_date_str = row[4]  # Assuming nextFuture is a string representing a date
+            # future_time_str = row[5]       # Assuming futureTime is a string representing time
 
-            # Convert next_future_date_str and future_time_str to datetime objects
-            year = int(next_future_date_str.split('년')[0].strip())
-            month = int(next_future_date_str.split('년')[1].split('월')[0].strip())
-            day = int(next_future_date_str.split('월')[1].split('일')[0].strip())
+            # # Convert next_future_date_str and future_time_str to datetime objects
+            # year = int(next_future_date_str.split('년')[0].strip())
+            # month = int(next_future_date_str.split('년')[1].split('월')[0].strip())
+            # day = int(next_future_date_str.split('월')[1].split('일')[0].strip())
 
-            hour, minute = map(int, future_time_str.split(':'))
-            reservation_datetime = datetime(year, month, day, hour, minute)
+            # hour, minute = map(int, future_time_str.split(':'))
+            # reservation_datetime = datetime(year, month, day, hour, minute)
 
             # Combine the date and time to get the complete datetime
             # reservation_datetime = datetime.combine(next_future_datetime, future_time_datetime.time())
-            print(f'current_datetime: {current_datetime}, reservation_datetime: {reservation_datetime}')
+            print(f'current_datetime: {current_datetime}, selectedDay: {selectedDay}')
+            
+            # ! 코드 수정: 로그인 진행 시간을 09:00로 지정
 
-            if current_datetime >= reservation_datetime:
+            if current_datetime.day == selectedDay.day and current_datetime.hour == 9: # 테스트 시에 and 이후 제거
                 # Perform the login_test method (or any other action you want)
                 cookies, elapsed_time = login_test.login_test(
                     "https://www.debeach.co.kr/",
-                    row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]
+                    row[1], row[2], row[3], row[5], row[6], row[7], row[8], row[9], row[10], row[11]
                 )
                 # After performing the action, delete the reservation row
                 delete_reservation_data(row[0])
@@ -53,18 +57,18 @@ app = Flask(__name__)
 CORS(app)
 
 # Function to insert data into the Reservation table
-def insert_reservation_data(id, pw, personnel, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck):
+def insert_reservation_data(id, pw, personnel, selectedDay, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck):
     conn = sqlite3.connect("C:/golf_db/golf_db.db")
     cursor = conn.cursor()
 
     # Prepare the SQL query
     query = """
-    INSERT INTO Reservation (uid, upw, personnel, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO Reservation (uid, upw, personnel, selectedDay, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
 
     # Execute the query with the data provided
-    cursor.execute(query, (id, pw, personnel, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck))
+    cursor.execute(query, (id, pw, personnel, selectedDay, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck))
 
     # Commit the changes and close the connection
     conn.commit()
@@ -116,6 +120,7 @@ def reservation_route():
         id = request.form.get('id')
         pw = request.form.get('pw')
         personnel = request.form.get('personnel')
+        selectedDay = request.form.get('selectedDay')
         nextFuture = request.form.get('nextFuture')
         nextSaturday = request.form.get('nextSaturday')
         nextSunday = request.form.get('nextSunday')
@@ -125,7 +130,7 @@ def reservation_route():
         wednesdayCheck = request.form.get('wednesdayCheck')
         
         # sqlite db 연결
-        insert_reservation_data(id, pw, personnel, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck)
+        insert_reservation_data(id, pw, personnel, selectedDay, nextFuture, futureTime, nextSaturday, saturdayTime, nextSunday, sundayTime, wednesdayCheck)
 
         return jsonify({'success': True}), 200
         
@@ -148,13 +153,14 @@ def reservation_table():
                 'uid': row[1],
                 'upw': row[2],
                 'personnel': row[3],
-                'nextFuture': row[4],
-                'futureTime': row[5],
-                'nextSaturday': row[6],
-                'saturdayTime': row[7],
-                'nextSunday': row[8],
-                'sundayTime': row[9],
-                'wednesdayCheck': row[10],
+                'selectedDay' : row[4],
+                'nextFuture': row[5],
+                'futureTime': row[6],
+                'nextSaturday': row[7],
+                'saturdayTime': row[8],
+                'nextSunday': row[9],
+                'sundayTime': row[10],
+                'wednesdayCheck': row[11],
             }
             reservations.append(reservation)
 
@@ -174,6 +180,7 @@ def login_route(): # 스케줄링 기능 없이 로그인 및 예약 테스트
         id = request.form.get('id')
         pw = request.form.get('pw')
         personnel = request.form.get('personnel')
+        selectedDay = request.form.get('selectedDay')
         nextFuture = request.form.get('nextFuture')
         nextSaturday = request.form.get('nextSaturday')
         nextSunday = request.form.get('nextSunday')
@@ -208,7 +215,7 @@ def reservation_cancel_route(id):
 
 
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(check_and_delete_reservations, trigger='cron', second='0')
+scheduler.add_job(check_and_delete_reservations, trigger='cron', hour='*', minute='0', second='0')
 scheduler.start()
         
 if __name__ == '__main__':
